f7347be6ee44c4ef48eeac07f4707f3eb360788e
spowers42 <spowers.42@gmail.com>
2014-05-10 17:30:01 -0400

    fixed labes

--- plot4.R
+++ plot4.R
@@ -5,13 +5,13 @@ png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
 #plot 1
 plot(1:2880,as.numeric(as.character(new_data$Global_active_power)), type="l", xlim=c(0, 2880), 
-     ylab="GlobalActive Power (kilowatts)", xaxt="n", main="" )
+     ylab="GlobalActive Power (kilowatts)", xaxt="n", main="", xlab="")
 axis(2, at=c(0,2,4,6))
 axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
 
 #plot 2
 plot(1:2880,as.numeric(as.character(new_data$Voltage)), type="l", xlim=c(0, 2880), 
-     ylab="voltage", xaxt="n", main="" )
+     ylab="voltage", xaxt="n", main="",xlab="" )
 axis(2, at=c(234,236,238,240,242,244,246), labels=c(234,'',238,'',242,'',246))
 axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
 
@@ -27,7 +27,7 @@ legend("topright", c("sub metering 1", "sub metering 2", "sub metering 3"),
 axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
 #plot 4
 plot(1:2880,as.numeric(as.character(new_data$Global_reactive_power)), type="l", xlim=c(0, 2880), 
-     ylab="Global Reactive Power", xaxt="n", main="" )
+     ylab="Global Reactive Power", xaxt="n", main="",xlab="" )
 #axis(2, at=c(0,2,4,6))
 axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
 
