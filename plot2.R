9fc6d45ac636cb1c106f5629010c652a32d27acf
spowers42 <spowers.42@gmail.com>
2014-05-10 17:28:52 -0400

    fixed label

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ new_data <- subset(mydata, mydata$Date == "1/2/2007"| mydata$Date == "2/2/2007")
 
 png("plot2.png", width=480, height=480)
 plot(1:2880,as.numeric(as.character(new_data$Global_active_power)), type="l", xlim=c(0, 2880), 
-     ylab="GlobalActive Power (kilowatts)", xaxt="n", main="" )
+     ylab="GlobalActive Power (kilowatts)", xaxt="n", main="",xlab="")
 axis(2, at=c(0,2,4,6))
 axis(1, at=c(0,1440,2880), labels=c("Thu", "Fri", "Sat"))
 dev.off()
